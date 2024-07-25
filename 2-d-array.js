2c13986898fa5eb3303b736231fe1004373dc406
Akshay <akshayvgupta7@gmail.com>
2024-07-25 20:20:40 +0530

    convertToNDarray

---
+++ 2-d-array.js
@@ -0,0 +1,29 @@
+
+const convertTo2DArray = (arr,n)=>{
+    let tmpArr=[];
+    let tmpCount=0;
+    let res = [];
+    for(let i=0;i<arr.length;){
+        if(tmpCount<n){
+            tmpArr.push(arr[i]);
+            tmpCount++;
+            i++;
+        }
+        else{
+            console.log("tmpArr:", tmpArr);
+            res.push(tmpArr);
+            tmpCount=0;
+            tmpArr = [];
+        }
+    }
+    if(tmpArr){
+        res.push(tmpArr);
+    }
+
+    return res;
+}
+
+const arr = [1,2,3,4,5,6,7];
+const n =3;
+console.log(convertTo2DArray(arr,n));
+
